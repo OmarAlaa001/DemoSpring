36344f37cdcaf7eb1d1c5f5cedb374f87822b882
Hossam Ayman <hhasanelbadry2000@gmail.com>
2025-11-12 22:47:01 +0200

    adding lombok dependencies

--- src/main/java/com/example/Student/Controller/StudentController.java
+++ src/main/java/com/example/Student/Controller/StudentController.java
@@ -3,6 +3,7 @@ package com.example.Student.Controller;
 
 import com.example.Student.Service.StudentService;
 import com.example.Student.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,11 +13,29 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/students")
 public class StudentController {
-    private final StudentService studentService;
 
-    public StudentController(StudentService studentService) {
-        this.studentService = studentService;
-    }
+    //3 types of Injection
+
+    //1- Field Injection (inject without testing (spring team not recommend it) (organization can use this approach))
+    //(Eager Field)
+    @Autowired
+    private StudentService studentService;
+
+    //2- Constructor Injection (inject with testing) --(why)--> (arguments ==> all required) (cannot any argument to be null) ==> help in testing
+    //(Eager Field)
+    //private StudentService studentService;
+    //@Autowired
+    //public StudentController(/*active*/StudentService studentService) {
+    //    this.studentService = studentService;
+    //}
+
+    //3- Setter Injection (injection -> Lazy Field)
+    //private StudentService studentService;
+    //@Autowired
+    //public void setStudentService(StudentService studentService) {
+    //    this.studentService = studentService;
+    //}
+
 
     @GetMapping
     public List<Student> getAllStudents() {
